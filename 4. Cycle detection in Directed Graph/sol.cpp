fa0c8188b634609f198d75c407cb6966b27ed4c3
Sahil Lather <sahillather002@gmail.com>
2023-06-14 07:47:55 +0530

    Update sol.cpp

--- 4. Cycle detection in Directed Graph/sol.cpp
+++ 4. Cycle detection in Directed Graph/sol.cpp
@@ -23,7 +23,6 @@ int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
     for(int i=0;i<edges.size();i++){
         int u=edges[i].first;
          int v=edges[i].second;
-        
         adj[u].push_back(v);
     }
     //call dfs for all components
